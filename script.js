975ba5cfca580dc459c014cdec3304c8bbdf80ab
Jinesha Das <81145991+jineshdas@users.noreply.github.com>
2023-07-01 13:32:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function calculateTime(){
     var dayNames= ["SUN","MON","TUE","WED","THU","FRI","SAT"]
 
     hour=hour%12;
-    hour=hour?12:'12';
+    hour=hour==12?'12':hour;
     hour=hour<10?'0'+hour:hour;
     minute=minute<10?'0'+minute:minute;
     second=second<10?'0'+second:second;
